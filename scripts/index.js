72d957dd1cf9bff42bab53303756f496214ee687
Valentina Karpova <bfeatb@mail.ru>
2021-07-25 00:46:52 +0100

    style: add multiple close and open rules

--- scripts/index.js
+++ scripts/index.js
@@ -8,18 +8,37 @@ let popupForm = document.querySelector(".popup__form");
 let nameInput = popupForm.querySelector("input[name='name']");
 let descriptionInput = popupForm.querySelector("input[name='description']");
 
-let closeButton = document.querySelector(".popup__close");
+let closeButton = document.querySelector(".js-close-popup");
 
-//Open popup
+//Open popups
+
+document.addEventListener("click", function(e) {
+  if (e.target.classList.contains("js-open-popup")) {
+    openPopup(e.target.dataset.popupid);
+  }
+});
+
+function openPopup(id) {
+  document.getElementById(id).classList.add("popup_opened");
+}
+
+//Add data to the edit form when a popup is opening
+
 function onClickEditButton(){
-  editPopup.classList.add("popup_opened");
   nameInput.value = nameProfile.textContent;
   descriptionInput.value = descriptionProfile.textContent;
 }
 
-//Close popup
-function onClickCloseButton(){
-  editPopup.classList.remove("popup_opened");
+//Close popups
+
+document.addEventListener("click", function(e) {
+  if (e.target.classList.contains("js-close-popup")) {
+    closePopup(e.target.dataset.popupid);
+  }
+});
+
+function closePopup(id) {
+  document.getElementById(id).classList.remove("popup_opened");
 }
 
 //Send data from popup to profile.
@@ -27,12 +46,18 @@ function formSubmitHandler (evt) {
   evt.preventDefault();
   nameProfile.textContent = nameInput.value;
   descriptionProfile.textContent = descriptionInput.value;
-  onClickCloseButton();
+  closePopup(id);
+}
+
+//Add a new card.
+function formAddCard (evt) {
+  evt.preventDefault();
+  closePopup(id);
 }
 
 popupForm.addEventListener('submit', formSubmitHandler);
+closeButton.addEventListener("click", closePopup);
 editButton.addEventListener("click", onClickEditButton);
-closeButton.addEventListener("click", onClickCloseButton);
 
 
 
