27ca8715e00e636f57de0494f0a38301345285c7
Valentina Karpova <bfeatb@mail.ru>
2021-08-18 11:38:09 +0100

    refactor: rearrange initial-cards

---
+++ scripts/initial-cards.js
@@ -0,0 +1,37 @@
+const initialCards = [
+  {
+    name: "Санкт-Петербург",
+    link: "./images/catlamp_SPetersburgh.jpg",
+    alt: "Кот в Петербурге"
+  },
+
+  {
+    name: "Москва",
+    link: "./images/lampcat_Moscow.jpg",
+    alt: "Кот в Москве"
+  },
+
+  {
+    name: "Тверь",
+    link: "./images/lampcat_tver.jpg",
+    alt: "Кот в Твери"
+  },
+
+  {
+    name: "Яндекс",
+    link: "./images/lampcat_Yandex.jpg",
+    alt: "Кот в Яндексе"
+  },
+
+  {
+    name: "Кудыкина гора",
+    link: "./images/lampcat_Kmountain.jpg",
+    alt: "Кот на Кудыкиной горе"
+  },
+
+  {
+    name: "Тула и памятник тульскому прянику",
+    link: "./images/catlamp_Tula.jpg",
+    alt: "Кот в Туле"
+  },
+];
