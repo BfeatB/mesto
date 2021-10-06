3fe908a9f3fc2d106fa6221ca345a0a40d6f6b94
Valentina Karpova <bfeatb@mail.ru>
2021-10-06 10:39:06 +0100

    style: displace constants

--- src/components/PopupWithImage.js
+++ src/components/PopupWithImage.js
@@ -3,15 +3,13 @@ import { Popup } from "./Popup.js";
 export class PopupWithImage extends Popup {
     constructor(selector) {
         super(selector);
+        this._popupImg = this._popupEl.querySelector(".popup__img-gallery");
+        this._popupCaption = this._popupEl.querySelector(".popup__figcaption");
     }
 
     open({src, alt}) {
-        super.open();
-        const popupImg = this._popupEl.querySelector(".popup__img-gallery");
-        const popupCaption = this._popupEl.querySelector(".popup__figcaption");
-
-        popupImg.setAttribute("src", src);
-        popupImg.setAttribute("alt", alt);
-        popupCaption.textContent = alt;
+        super.open();this._popupImg.setAttribute("src", src);
+        this._popupImg.setAttribute("alt", alt);
+        this._popupCaption.textContent = alt;
     }
 }
